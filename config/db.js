ce9835d58d4faf71045edea074a0813509764e37
Nahiankhondokar <nahiankhondokarphp@gmail.com>
2022-06-13 06:43:06 -0700

    admin Modeling done

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectMongoDB = async () => {
 
     try{
 
-        let connect = await mongoose.connect('mongodb+srv://nahian:1234567890@testingcluster.mg6ys.mongodb.net/first-express?retryWrites=true&w=majority');
+        let connect = await mongoose.connect(process.env.MONGO_DB);
         console.log(`mongoose server is connected on HOST ${connect.connection.host}`.bgBlue.white);
     }catch(err){
         console.log(`${err}`.bgRed.white);
--- models/AdminModel.js
+++ models/AdminModel.js
@@ -1,9 +1,42 @@
 const mongoose = require('mongoose');
 
 
-const adminModel = () => {
-
-}
+const adminModel = mongoose.Schema({
+    name : {
+        type : String,
+        required : [true, "Name Feild is required"]
+    },
+    email : {
+        type : String,
+        required : [true, "Email Feild is required"],
+        unique : true
+    },
+    cell : {
+        type : String,
+        required : [true, "cell Feild is required"],
+        unique : true,
+        match : /^(01|8801|+8801)[0-9]{9}$/,
+        unique : true
+    },
+    username : {
+        type : String,
+        required : [true, "username Feild is required"],
+        maxLength : 10,
+        minLength : 5
+    },
+    location : {
+        type : String,
+        required : false,
+        default : "Dhaka"
+    },
+    skill : {
+        type : String,
+        required : false
+    },
+    password : {
+        type : String
+    }
+});
 
 
 
