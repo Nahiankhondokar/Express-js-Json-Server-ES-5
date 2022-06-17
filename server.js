6f1f48d60788e837a4df210503f8ad0ec18bd2e3
Nahiankhondokar <nahiankhondokarphp@gmail.com>
2022-06-17 11:25:11 +0600

    express single file upload by multer done

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const mongoDBConnection = require('./config/db');
 // mongdoDB Connection
 mongoDBConnection();
 
+// multer config
+const multer = require('multer');
+const upload = multer({
+    dest : './media/users'
+});
+
 
 // environment 
 const PORT = process.env.SERVER_PORT;
@@ -14,6 +20,12 @@ const PORT = process.env.SERVER_PORT;
 // Request body init
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
+
+
+// file upload for single img
+app.post('/file-upload', upload.single('image'), (req, res) => {
+    res.send('file upload');
+});
 
 // student route connection
 app.use('/api/students', require('./routes/student'));
@@ -23,6 +35,8 @@ app.use('/api/admins', require('./routes/admin'));
 
 // Teacher Route Connection
 app.use('/api/teachers', require('./routes/teacher'));
+
+
 
 
 app.listen(PORT, () => console.log(`Server is running on post ${PORT}`.bgGreen.white));
