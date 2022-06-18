5eed4ceaceb5a4cf71bceba2d9bb793b377f3694
Nahiankhondokar <nahiankhondokarphp@gmail.com>
2022-06-18 15:42:01 +0600

    add multer fields system

--- server.js
+++ server.js
@@ -13,14 +13,29 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination : (req, file, cb) => {
-        cb(null, './media/users/');
+        
+        if(file.fieldname == 'image'){
+            cb(null, './media/users/');
+        }else if(file.fieldname == 'cv'){
+            cb(null, './media/cv/');
+        }
+        
     },
     filename : (req, file, cb) => {
 
-        let ext = path.extname(file.originalname);
-        let fileName = Date.now() + Math.round(Math.random() * 100000) + ext;
-        // console.log(fileName);
-        cb(null, fileName);
+        if(file.fieldname == 'image'){
+            let ext = path.extname(file.originalname);
+            let fileName = Date.now() + Math.round(Math.random() * 100000) + ext;
+            // console.log(fileName);
+            cb(null, fileName);
+        }else if(file.fieldname == 'cv'){
+            
+            const date = new Date();
+            const fileName = date.getMonth() +'-'+ date.getDate() +'-'+ date.getFullYear() +'-'+ file.originalname;
+            cb(null, fileName);
+        }
+
+
     }
 });
 
@@ -28,12 +43,42 @@ const upload = multer({
     storage : storage,
     limits : (1024*1024),
     fileFilter : (req, file, cb) => {
-        if(file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg'){
-            cb(null, true);
-        }else{
-            cb(console.log('type does not match'));
+
+        if(file.fieldname == 'image'){
+            if(file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg'){
+                cb(null, true);
+            }else{
+                cb(console.log('type does not match'));
+            }
+        }else if(file.fieldname == 'cv'){
+            if(file.mimetype == 'application/pdf' ){
+                cb(null, true);
+            }else{
+                cb(console.log('pdf type does not match'));
+            }
         }
+
     }
+});
+
+const cpUpload = upload.fields([
+    {
+        name : 'image',
+        maxCount : 10
+    }, 
+    {
+        name : 'cv',
+        maxCount : 1
+    }
+]);
+
+
+// file upload for multiple img
+app.post('/file-upload', cpUpload, (req, res) => {
+    
+    console.log(req.files);
+    res.send('file uploaded');
+
 });
 
 
@@ -43,19 +88,6 @@ const PORT = process.env.SERVER_PORT;
 // Request body init
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
-
-
-// // file upload for single img
-// app.post('/file-upload', upload.single('image'), (req, res) => {
-//     res.send('file upload');
-// });
-
-// file upload for multiple img
-app.post('/file-upload', upload.array('image'), (req, res) => {
-    
-    console.log(req.files);
-
-});
 
 // student route connection
 app.use('/api/students', require('./routes/student'));
